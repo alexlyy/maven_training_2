a5f4cf43afd553c0d5982be4ace93073b7ef8c23
alexlyy <alexandre.ly@epita.fr>
2021-06-17 12:19:48 +0200

    MVN compile work

--- lernejo-tester/src/main/java/fr/lernejo/tester/api/TestMethod.java
+++ lernejo-tester/src/main/java/fr/lernejo/tester/api/TestMethod.java
@@ -1,6 +1,12 @@
 package fr.lernejo.tester.api;
-@Retention(RetentionPolicy.RUNTIME) // (1)
-@Target(ElementType.METHOD) // (2)
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
 
 public @interface TestMethod {
 }
